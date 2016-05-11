59179f8ef80ac4a32726ec0fab64ef34bdd5dfe5
Alex Polchert <anp6vq@virginia.edu>
2016-05-11 20:42:32 +0200

    api skeletons

---
+++ imports/api/expenses/expenses.js
@@ -0,0 +1,6 @@
+import faker from 'faker';
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { Factory } from 'meteor/dburles:factory';
+
+export const Expenses = new Mongo.Collection('Expenses');
