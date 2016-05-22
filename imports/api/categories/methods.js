a693d0f3cd2472ebf3633b6296970edbee6d7841
Alex Polchert <anp6vq@virginia.edu>
2016-05-22 19:16:37 +0200

    categories find by global and user specific

--- imports/api/categories/methods.js
+++ imports/api/categories/methods.js
@@ -1,13 +1,16 @@
 import { Categories } from './categories';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
+import { Meteor } from 'meteor/meteor';
 
 export const insertCategory = new ValidatedMethod({
   name: 'category.insert',
   validate: new SimpleSchema({
-    title: { type: String },
+    value: { type: String },
+    label: {type : String},
   }).validator(),
   run(document) {
+    console.log(`inserting new category meteor user id is ${Meteor.userId()}`)
     Categories.insert(document);
   },
 });
@@ -22,6 +25,15 @@ export const updateCategory = new ValidatedMethod({
     Categories.update(_id, { $set: update });
   },
 });
+
+export const getCategories = new ValidatedMethod({
+  name: 'categories.retrieve',
+  run : {
+    Categories.find({
+      userId : {$in: ["admin", Meteor.userId()]} //admin user hardcoded on seeding - so find all global and user specific categories
+    }).fetch()
+  }
+})
 
 export const removeCategory = new ValidatedMethod({
   name: 'category.remove',
