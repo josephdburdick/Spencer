41be83385c7b1895acb909ed7928c1c409221b24
Kay Taiwo <kay_taylor@outlook.com>
2016-06-19 05:09:43 +0100

    filtering by year, quarter and month now happens via underscore.

--- imports/ui/components/expenses-list.js
+++ imports/ui/components/expenses-list.js
@@ -8,7 +8,46 @@ export class ExpensesList extends React.Component {
     super(props);
   }
   render () {
-    const where_props = {};
+    const expenses = _.filter(this.props.expenses, (expense) => {
+      const year = this.props.year;
+      const quarter = this.props.quarter;
+      const month = this.props.month;
+      if (year) {
+        if (quarter) {
+          switch(Number.parseInt(quarter)) {
+            case 1:
+              return (expense.year == Number.parseInt(year) && (expense.month == 1 || expense.month == 2 || expense.month == 3));
+            case 2:
+              return (expense.year == Number.parseInt(year) && (expense.month == 4 || expense.month == 5 || expense.month == 6));
+            case 3:
+              return (expense.year == Number.parseInt(year) && (expense.month == 7 || expense.month == 8 || expense.month == 9));
+            case 4:
+              return (expense.year == Number.parseInt(year) && (expense.month == 10 || expense.month == 11 || expense.month == 12));
+          }
+        }
+        if (month) {
+          return (expense.year == Number.parseInt(year) && expense.month == Number.parseInt(month));
+        }
+        return (expense.year == Number.parseInt(year));
+      }
+      if (quarter) {
+        switch(Number.parseInt(quarter)) {
+          case 1:
+            return (expense.month == 1 || expense.month == 2 || expense.month == 3);
+          case 2:
+            return (expense.month == 4 || expense.month == 5 || expense.month == 6);
+          case 3:
+            return (expense.month == 7 || expense.month == 8 || expense.month == 9);
+          case 4:
+            return (expense.month == 10 || expense.month == 11 || expense.month == 12);
+        }
+      }
+      if (month) {
+        return (expense.month == Number.parseInt(month));
+      }
+      return true; 
+    });
+    /*const where_props = {};
     let expenses = this.props.expenses;
     if (this.props.year) {
        where_props.year = Number.parseInt(this.props.year);
@@ -22,7 +61,7 @@ export class ExpensesList extends React.Component {
     if (Object.keys(where_props).length > 0) {
       expenses = _.where(this.props.expenses, where_props);
       // expenses = _.filter(this.props.expenses, function(expense){ return expense.year == Number.parseInt(year); });
-    }
+    }*/
     console.log(`ExpensesList.render: year => ${this.props.year}, quarter => ${this.props.quarter}, month => ${this.props.month}, sort => ${this.props.sort}`);
     return (
       expenses.length > 0 ? (
