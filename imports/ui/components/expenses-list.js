a174628c8f2137bf149abd941e4584d7ca38c16e
Kay Taiwo <kay_taylor@outlook.com>
2016-06-19 04:31:06 +0100

    filtering by month now happens via underscore.

--- imports/ui/components/expenses-list.js
+++ imports/ui/components/expenses-list.js
@@ -8,10 +8,19 @@ export class ExpensesList extends React.Component {
     super(props);
   }
   render () {
+    const where_props = {};
     let expenses = this.props.expenses;
-    const year = this.props.year;
-    if (year) {
-      expenses = _.where(this.props.expenses, {year: Number.parseInt(year)});
+    if (this.props.year) {
+       where_props.year = Number.parseInt(this.props.year);
+    }
+    if (this.props.quarter) {
+       // where_props.year = Number.parseInt(this.props.year);
+    }
+    if (this.props.month) {
+       where_props.month = Number.parseInt(this.props.month);
+    }
+    if (Object.keys(where_props).length > 0) {
+      expenses = _.where(this.props.expenses, where_props);
       // expenses = _.filter(this.props.expenses, function(expense){ return expense.year == Number.parseInt(year); });
     }
     console.log(`ExpensesList.render: year => ${this.props.year}, quarter => ${this.props.quarter}, month => ${this.props.month}, sort => ${this.props.sort}`);
